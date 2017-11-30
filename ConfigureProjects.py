49369a72c8e6303f8c6eb23ca928a7bed8d69317
Steven Sell <ssell@ocularinteractive.com>
2017-11-30 00:25:03 -0500

    Potential Linux makefile steps

--- ConfigureProjects.py
+++ ConfigureProjects.py
@@ -9,25 +9,23 @@ import shutil
 
 from sys import platform
 
-COMPILER_WIN       = "msvc140"                        # Compiler identifier added to library output (example: API_msvc140.dll)
+COMPILER_WIN       = "msvc141"                        # Compiler identifier added to library output (example: API_msvc141.dll)
 COMPILER_LINUX     = ""                               # Compiler identifier added to library output
 COMPILER_OSX       = ""                               # Compiler identifier added to library output
 GENERATOR_WIN32    = "Visual Studio 15 2017"          # CMake generator to use for Windows x86
 GENERATOR_WIN64    = "Visual Studio 15 2017 Win64"    # CMake generator to use for Windows x64
-GENERATOR_LINUX32  = ""                               # CMake generator to use for Linux
-GENERATOR_LINUX64  = ""                               # CMake generator to use for Linux
-GENERATOR_OSX32    = ""                               # CMake generator to use for OS X
-GENERATOR_OSX64    = ""                               # CMake generator to use for OS X 
+GENERATOR_LINUX    = "Unix Makefiles"                 # CMake generator to use for Linux
+GENERATOR_OSX      = "Xcode"                          # CMake generator to use for OS X
 API_DIR            = "API/"                           # Main directory for the API project  
 BUILD_DIR          = "build/"                         # Build subdirectory within project directories
 BUILD_SUBDIR_WIN32 = "VS2017_x86/"                    # Subdirectory within build for Windows x86 projects
 BUILD_SUBDIR_WIN64 = "VS2017_x64/"                    # Subdirectory within build for Windows x64 projects
-BUILD_SUBDIR_LINUX = ""                               # Subdirectory within build for Linux projects
-BUILD_SUBDIR_OSX   = ""                               # Subdirectory within build for OS X projects
+BUILD_SUBDIR_LINUX = "linux"                          # Subdirectory within build for Linux projects
+BUILD_SUBDIR_OSX   = "osx"                            # Subdirectory within build for OS X projects
 
-# "cmake -DPARAM_COMPILER=... -DPARAM_ARCH=... -G generator outputpath"
-CMAKE_COMMANDS32  = ["cmake", "-DPARAM_COMPILER=", "-DPARAM_ARCH=x86", "-G", "", "../../"]
-CMAKE_COMMANDS64  = ["cmake", "-DPARAM_COMPILER=", "-DPARAM_ARCH=x64", "-G", "", "../../"]
+# "cmake -DPARAM_COMPILER=... -DPARAM_ARCH=... ...additionalarguments... -G generator outputpath"
+CMAKE_COMMANDS32  = ["cmake", "-DPARAM_COMPILER=", "-DPARAM_ARCH=x86", "", "-G", "", "../../"]
+CMAKE_COMMANDS64  = ["cmake", "-DPARAM_COMPILER=", "-DPARAM_ARCH=x64", "", "-G", "", "../../"]
 
 # ---------------------------------------------------------------------- #
 # - Context manager for changing the current working directory         - #
@@ -54,11 +52,17 @@ def GetWindowsCommand():
     logger.info("Detected Windows operating system.")
     CMAKE_COMMANDS32[1] += COMPILER_WIN
     CMAKE_COMMANDS64[1] += COMPILER_WIN
-    CMAKE_COMMANDS32[4]  = GENERATOR_WIN32
-    CMAKE_COMMANDS64[4]  = GENERATOR_WIN64
+    CMAKE_COMMANDS32[5]  = GENERATOR_WIN32
+    CMAKE_COMMANDS64[5]  = GENERATOR_WIN64
 
 def GetLinuxCommand():
     logger.info("Detected Linux operating system.")
+    CMAKE_COMMANDS32[1] += COMPILER_LINUX
+    CMAKE_COMMANDS64[1] += COMPILER_LINUX
+    CMAKE_COMMANDS32[3]  = "-DCMAKE_CXX_COMPILER=g++ -DCMAKE_CXX_FLAGS=-m32"
+    CMAKE_COMMANDS64[3]  = "-DCMAKE_CXX_COMPILER=g++ -DCMAKE_CXX_FLAGS=-m64"
+    CMAKE_COMMANDS32[5]  = GENERATOR_LINUX
+    CMAKE_COMMANDS64[5]  = GENERATOR_LINUX
 
 def GetOSXCommand():
     logger.info("Detected OS X operating system.")
@@ -89,7 +93,7 @@ def SetupAPI(cmakeCommand, useShell, buildDir, command):
                     return
 
     with cd(buildPath):
-        logger.info("\tcmake " + command[1] + " " + command[2] + " " + command[3] + " \"" + command[4] + "\" " + command[5] + "")
+        logger.info("\tcmake " + command[1] + " " + command[2] + " " + command[3] + "  " + command[4] + " \"" + command[5] + "\" " + command[6] + "")
         retVal = subprocess.check_call(command, stderr=subprocess.STDOUT, shell=useShell)
 
         if retVal == 0:
