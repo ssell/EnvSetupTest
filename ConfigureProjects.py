61ead56444d8c7f5924af6bf71847090f7ecc4e5
Steven Sell <ssell@ocularinteractive.com>
2017-12-14 00:44:59 -0500

    Added vendor retrieval to configuration script

--- ConfigureProjects.py
+++ ConfigureProjects.py
@@ -7,6 +7,8 @@ import subprocess
 import argparse
 import sys
 import shutil 
+import urllib.request
+import zipfile 
 
 from sys import platform
 
@@ -17,14 +19,17 @@ GENERATOR_WIN32      = "Visual Studio 15 2017"          # CMake generator to use
 GENERATOR_WIN64      = "Visual Studio 15 2017 Win64"    # CMake generator to use for Windows x64
 GENERATOR_LINUX      = "Unix Makefiles"                 # CMake generator to use for Linux
 GENERATOR_OSX        = "Xcode"                          # CMake generator to use for OS X
-API_DIR              = "API/"                           # Main directory for the API project  
+API_DIR              = "API/"                           # Main directory for the API project
 BUILD_DIR            = "build/"                         # Build subdirectory within project directories
 BUILD_SUBDIR_WIN32   = "VS2017_x86/"                    # Subdirectory within build for Windows x86 projects
 BUILD_SUBDIR_WIN64   = "VS2017_x64/"                    # Subdirectory within build for Windows x64 projects
 BUILD_SUBDIR_LINUX32 = "linux_x86/"                     # Subdirectory within build for Linux x86 projects
 BUILD_SUBDIR_LINUX64 = "linux_x64/"                     # Subdirectory within build for Linux x64 projects
 BUILD_SUBDIR_OSX32   = "osx_x86/"                       # Subdirectory within build for OS X x86 projects
-BUILD_SUBDIR_OSX64   = "osx_x64/"                       # Subdirectory within build for OS X x64 projects
+BUILD_SUBDIR_OSX64   = "osx_x64/"                       # Subdirectory within build for OS X x64 projects  
+VENDOR_DIR           = "vendors/"                        # 
+VENDOR_URL           = "https://s3.amazonaws.com/phoenixrenderer/vendor/PhoenixVendors_Latest.zip"
+VENDOR_TEMP          = "vendors_temp.zip"
 
 # "cmake -DPARAM_COMPILER=... -DPARAM_ARCH=... ...additionalarguments... -G generator outputpath"
 CMAKE_COMMANDS32  = ["cmake", "-DPARAM_COMPILER=", "-DPARAM_ARCH=x86", "", "-G", "", "../../"]
@@ -47,11 +52,31 @@ class cd:
         os.chdir(self.savedPath)
         logger.info("cd '" + self.savedPath + "'")
     
+# ---------------------------------------------------------------------- #    
+# - Make Directory helper                                              - #
+# ---------------------------------------------------------------------- #
+
+def MakeDirectory(dirName):
+    if not os.path.exists(dirName):
+        logger.info("Directory not found. Creating directory '" + dirName + "' ...")
+        try:
+            os.makedirs(dirName)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                logger.error("Failed to create directory '" + dirName + "'")
+                return False
+
+
 # ---------------------------------------------------------------------- #
 # - Clean Command                                                      - #
 # ---------------------------------------------------------------------- #
 
 def Clean():
+    # Clean Vendors
+    if os.path.exists(VENDOR_DIR):
+        logger.info("Cleaning '" + VENDOR_DIR + "' ...")
+        shutil.rmtree(VENDOR_DIR)
+
     # Clean API
     if os.path.exists(API_DIR + BUILD_DIR):
         logger.info("Cleaning '" + API_DIR + BUILD_DIR + "' ...")
@@ -88,6 +113,39 @@ def GetOSXCommand():
     logger.info("Detected OS X operating system.")
 
 # ---------------------------------------------------------------------- #
+# - Pull Third Party Libraries                                         - #
+# ---------------------------------------------------------------------- #
+
+def GetVendors():
+    logger.info("Retrieving third-party dependencies ...")
+    
+    if os.path.exists(VENDOR_DIR):
+        logger.info("Detected presence of '" + VENDOR_DIR + "' already. Skipping.")
+        return True
+
+    logger.info("Downloading '" + VENDOR_URL + "' to '" + VENDOR_TEMP + "' ...")
+    urllib.request.urlretrieve(VENDOR_URL, VENDOR_TEMP)
+
+    if os.path.isfile(VENDOR_TEMP) is False:
+        logger.error("Failed to download temporary vendor binaries.")
+        return False
+
+    logger.info("Extracting '" + VENDOR_TEMP + "' to '" + VENDOR_DIR + "' ...")
+    zipFile = zipfile.ZipFile(VENDOR_TEMP, 'r')
+    zipFile.extractall(".")
+    zipFile.close()
+
+    if os.path.exists(VENDOR_DIR) is False:
+        logger.error("Failed to extract vendor binaries.")
+        return False
+
+    logger.info("Extracted vendor binaries to '" + VENDOR_DIR + "'")
+    logger.info("Removing temporary file '" + VENDOR_TEMP + "'")
+    os.remove(VENDOR_TEMP)
+
+    return True
+
+# ---------------------------------------------------------------------- #
 # - API Project Setup                                                  - #
 # ---------------------------------------------------------------------- #
 
@@ -103,14 +161,8 @@ def SetupAPI(cmakeCommand, useShell, buildDir, command):
 
     logger.info("Verifying Build directory '" + buildPath + "'")
 
-    if not os.path.exists(buildPath):
-            logger.info("Build directory not found. Creating directory ...")
-            try:
-                os.makedirs(buildPath)
-            except OSError as e:
-                if e.errno != errno.EEXIST:
-                    logger.error("Failed to create build directory.")
-                    return False
+    if MakeDirectory(buildPath) is False:
+        return False;
 
     with cd(buildPath):
         logger.info(" ".join(command))
@@ -134,14 +186,8 @@ def SetupBuildAll(buildDir):
     logger.info("Configuring VS Build All ...")
     logger.info("Verifying Build All directory '" + buildDir + "'")
 
-    if not os.path.exists(buildDir):
-        logger.info("Build directory not found. Creating directory ...")
-        try:
-            os.makedirs(buildDir)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                logger.error("Failed to create build directory.")
-                return False
+    if MakeDirectory(buildDir) is False:
+        return False;
 
     with cd(buildDir):
         logger.info("cmake -DPARAM_COMPILER=" + COMPILER_WIN + " ../../")
@@ -194,6 +240,10 @@ if args.clean is True:
 
 if args.rebuild is True:
     Clean()
+
+if GetVendors() is False:
+    logger.error("Failed to retrieve vendor binaries. Exiting.")
+    exit(0)
 
 # ---------------------------------------------------------------------- #
 # - Operating system entry                                             - #
--- README.md
+++ README.md
@@ -9,5 +9,3 @@ Repository for testing the setup of a C++ development environment using:
 * Google Test
 * Some code coverage (TBD)
 * Coverity
-
-Though CMake will be used for testing purposes, actual development is in Visual Studio 2017.
